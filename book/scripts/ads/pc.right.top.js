7dc5f6003d22abfe12482d5ed5beaada76515295
aiservice <zaixiankefu365@gmail.com>
2020-06-24 13:47:57 +0800

    add js

--- book/scripts/ads/pc.right.top.js
+++ book/scripts/ads/pc.right.top.js
@@ -6,4 +6,6 @@ if(typeof site_enabled_b != "undefined" && site_enabled_b){
     loadExoAds("pc_right_top");
 } else if(typeof site_enabled_t != "undefined" && site_enabled_t){
     loadTerraAds("pc_right_top");
+} else if(typeof site_enabled_alimama != "undefined" && site_enabled_alimama){
+    loadAlimama("pc_right_top");
 }
