db9b688b0f5bcc9f9e10a80e0f68ab1c0773db70
Yang Wu (yangwu3) <yangwu3@cisco.com>
2020-07-23 13:29:04 +0800

    add feiku6 js

---
+++ book/scripts/index/site/19291.js
@@ -0,0 +1,23 @@
+var app_domain = "www.feiku6.com";var is_english = false;var app_link_email = "aikefu001@outlook.com";var site_id = 19291;var baid = "f50fb8859e2d48c51beafff64e7c000d";var gaid = "UA-40716497-3";var ctx_cdn = "//cdn.book.moujishu.com";var cdn_api = "//cdn.book.moujishu.com";g_data_ad_slot_auto = "2008793013";
+g_data_ad_slot_468 = "2008793013";
+g_data_ad_slot_recommend = "7657556062";
+g_data_ad_client = "ca-pub-4603891518763240";
+
+filterUrls = ["cate-5008","category-5008"];
+siteGUrls = ["xxx"];
+siteAlimamaUrls = ["feiku"];
+
+o_data_ad_300="";
+o_data_ad_300_right="";
+o_data_ad_mobile="8434150992547607260-539";
+o_data_ad_728="";
+siteOtherUrls = [];
+
+e_data_ad_300="3707943";
+e_data_ad_468="3707945";
+e_data_ad_160_600="3707947";
+e_data_ad_728="3801741";
+e_data_ad_mobile="3707953";
+siteEUrls = [];
+
+third_opacity_css  ="opacity: 0.3;"
