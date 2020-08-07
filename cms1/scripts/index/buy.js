d5adf5a2462e505f0554d607da81d7995f45130a
Yang Wu (yangwu3) <yangwu3@cisco.com>
2020-08-07 12:56:51 +0800

    add cms1

---
+++ cms1/scripts/index/buy.js
@@ -0,0 +1,18 @@
+try {
+  var $data = $(".article-title")
+  var title = $data.text();
+  if(title){
+    title = $.trim(title);
+    var subStr = title.match("《(.*)》");
+    if(subStr){
+      title = subStr[1];
+    }
+    var buy_url = "https://ai.taobao.com/search/index.htm?key="+encodeURIComponent(title)+"&pid=mm_16234744_1806700429_110511900427";
+    if($data.data("buy-url") != ""){
+      buy_url = $data.data("buy-url");
+    }
+    document.writeln('<div class="alert alert-info" role="alert"> 购买纸版：<a href="'+buy_url+'" target="_blank" class="alert-link">'+$data.text()+'</a> </div>');
+  }
+}catch (e) {
+  console.log(e);
+}
---
+++ cms1/scripts/index/site/1937.js
@@ -0,0 +1,14 @@
+var app_domain = "sunyunzhu.chinaoc.com.cn";var is_english = false;var site_id = 1937;var book_total_fee = 0.5;var baid = "72a1971b2927f5daa13d83c7d8613dfe";var gaid = "UA-40716497-11";b_data_ad_mobile = "u3689760";
+var link_email = "2482232786@qq.com";b_data_ad_336 = "u3689763";
+b_data_ad_336_xuanting = "u3689796";
+
+g_data_ad_slot_auto = "2215424712";
+g_data_ad_slot_recommend = "6053545297";
+g_data_ad_client = "ca-pub-4603891518763240";
+
+filterUrls = ["keyword=2003"];
+go_my_site=true;
+filterClsIds = [9321, 9320];
+siteGUrls = ["china"];
+siteBUrls = ["aiis","china"];
+
