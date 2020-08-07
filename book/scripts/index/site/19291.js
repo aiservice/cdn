a9209ff12abbe62d0e1715c8bb1de96975107dca
Yang Wu (yangwu3) <yangwu3@cisco.com>
2020-08-07 12:26:12 +0800

    add mall css

--- book/scripts/index/site/19291.js
+++ book/scripts/index/site/19291.js
@@ -1,4 +1,4 @@
-var app_domain = "www.feiku6.com";var is_english = false;var app_link_email = "aikefu001@outlook.com";var site_id = 19291;var baid = "f50fb8859e2d48c51beafff64e7c000d";var gaid = "UA-40716497-3";var ctx_cdn = "//cdn.book.moujishu.com";var cdn_api = "//cdn.book.moujishu.com";g_data_ad_slot_auto = "2008793013";
+var app_domain = "www.biquge.work";var is_english = false;var app_link_email = "aikefu001@outlook.com";var site_id = 19291;var baid = "f50fb8859e2d48c51beafff64e7c000d";var gaid = "UA-40716497-3";var ctx_cdn = "//cdn.book.moujishu.com";var cdn_api = "//cdn.book.moujishu.com";g_data_ad_slot_auto = "2008793013";
 g_data_ad_slot_468 = "2008793013";
 g_data_ad_slot_recommend = "7657556062";
 g_data_ad_client = "ca-pub-4603891518763240";
