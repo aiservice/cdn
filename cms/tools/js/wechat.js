2097b2a4fc21b02e9d4731dcd90bf0bc6dca1096
yangwu3 <yangwu3@cisco.com>
2020-09-02 13:36:40 +0800

    fix js

--- cms/tools/js/wechat.js
+++ cms/tools/js/wechat.js
@@ -5,35 +5,11 @@ $(function () {
         errorClass: 'help-block',
         focusInvalid: false,
         ignore: "",
-        debug: true,
-        rules: {
-            real_name: {required: true},
-            bank_account: {required: true},
-            cash_count: {required: true, digits: true},
-        },
-        messages: {
-            real_name: {required: "请添加提现账户"},
-            bank_account: {required: "请添加提现账户"},
-            cash_count: {required: "请填写提现金额", digits: "请正确填写提现金额"},
-        },
-        highlight: function (e) {
-            $(e).closest('.form-group').removeClass('has-info').addClass('has-error');
-        },
-        success: function (e) {
-            $(e).closest('.form-group').removeClass('has-error');
-            $(e).remove();
-        }
-    });
-    vform = $('#validation-form').validate({
-        errorElement: 'div',
-        errorClass: 'help-block',
-        focusInvalid: false,
-        ignore: "",
         rules: {
             wechat_url: {required: true, url: true},
         },
         messages: {
-            site_domain: {required: "请填写微信视频链接", url: "请正确填写微信视频链接,需包含http或者https"},
+            wechat_url: {required: "请填写微信视频链接", url: "请正确填写微信视频链接,需包含http或者https"},
         },
         highlight: function (e) {
             $(e).closest('.form-group').removeClass('has-info').addClass('has-error');
