9dc16d72b7f4dd3d3fae9260584ec210cd7be825
yangwu3 <yangwu3@cisco.com>
2020-09-11 15:55:17 +0800

    add bus

--- cms/scripts/index/site/1937.js
+++ cms/scripts/index/site/1937.js
@@ -1,5 +1,5 @@
-var app_domain = "sunyunzhu.chinaoc.com.cn";var is_english = false;var site_id = 1937;var book_total_fee = 0.5;var baid = "72a1971b2927f5daa13d83c7d8613dfe";var gaid = "UA-40716497-11";b_data_ad_mobile = "u3689760";
-var link_email = "2482232786@qq.com";b_data_ad_336 = "u3689763";
+var app_domain = "zan.igoodtv.com";var is_english = false;var site_id = 1937;var book_total_fee = 1;var baid = "72a1971b2927f5daa13d83c7d8613dfe";var link_email = "igoodtv@igoodtv.com";var gaid = "UA-40716497-11";b_data_ad_mobile = "u3689760";
+b_data_ad_336 = "u3689763";
 b_data_ad_336_xuanting = "u3689796";
 
 g_data_ad_slot_auto = "2215424712";
@@ -7,7 +7,9 @@ g_data_ad_slot_recommend = "6053545297";
 g_data_ad_client = "ca-pub-4603891518763240";
 
 filterUrls = ["keyword=2003"];
+go_my_site=true;
 filterClsIds = [9321, 9320];
 siteGUrls = ["aiis","good","china","shitou","soft","xiaoli","6tie","lzys","feiku","biquge"];
 siteBUrls = ["aiis","china"];
 
+third_opacity_css  ="opacity: 0.5;"
