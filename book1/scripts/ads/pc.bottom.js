8fe46d633d1839815620f82999f678a0508c17eb
yangwu3 <yangwu3@cisco.com>
2020-10-13 13:22:19 +0800

    add bus

--- book1/scripts/ads/pc.bottom.js
+++
@@ -1,5 +0,0 @@
-if(typeof site_enabled_b != "undefined" && site_enabled_b){
-    loadBaiduAds("pc_bottom");
-} else if(typeof site_enabled_other != "undefined" && site_enabled_other){
-    loadThirdAds("pc_bottom");
-}
