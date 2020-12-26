dd92b5a46a0e64c36eae7fe7099363422a092dd9
yangwu3 <yangwu3@cisco.com>
2020-12-26 17:29:54 +0800

    add toutiaoweb

--- cms/scripts/index/site/1964.js
+++ cms/scripts/index/site/1964.js
@@ -1,4 +1,4 @@
-var app_domain = "www.chinaoc.com.cn";var is_english = false;var site_id = 1964;var book_total_fee = 0.5;var baid = "74fc812e6fa07dc004517e2ef9ce340f";var gaid = "UA-40716497-7";b_data_ad_mobile = "u3689760";
+var app_domain = "www.toutiaoweb.com";var is_english = false;var site_id = 1964;var book_total_fee = 0.5;var baid = "74fc812e6fa07dc004517e2ef9ce340f";var gaid = "UA-40716497-7";b_data_ad_mobile = "u3689760";
 var link_email = "2482232786@qq.com";b_data_ad_336 = "u3689763";
 b_data_ad_336_xuanting = "u3689796";
 
@@ -7,8 +7,7 @@ g_data_ad_slot_recommend = "6053545297";
 g_data_ad_client = "ca-pub-4603891518763240";
 
 filterUrls = ["xxxxx"];
-filterSiteIds = [1937];
-siteGUrls = ["aiis","good","china","shitou","soft","xiaoli","6tie","lzys","feiku","biquge","ppt"];
+siteGUrls = ["toutiao"];
 siteBUrls = ["aiis","china"];
 
 go_my_site=false;
