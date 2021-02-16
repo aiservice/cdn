e23843a3ad8fe6875cc73f567f5f3d2e528d4bf6
yangwu3 <yangwu3@cisco.com>
2021-02-16 20:45:52 +0800

    fix js

--- cms/scripts/index/update.news.js
+++ cms/scripts/index/update.news.js
@@ -3,5 +3,21 @@ $(document).ready(function () {
 });
 
 function updateViewCount() {
-    $.post("/CsAjax.do?method=updateNewsViewCount", {"news_id": $("#news_id").val()}, function (datas) {});
+    $.post("/CsAjax.do?method=updateNewsViewCount", {"news_id": $("#news_id").val()}, function (datas) {
+        if (datas.enable_pay && typeof site_id != "undefined" && site_id === 1772) {
+            if (datas.enable_pay === "true") {
+                var $no_source = $("#no_source");
+                if ($no_source.length > 0) {
+                    $no_source.after(paySourceHtml());
+                    $no_source.remove();
+                }
+            } else {
+                var $pay_source = $("#pay_source");
+                if ($pay_source.length > 0) {
+                    $pay_source.after(noSourceHtml());
+                    $pay_source.remove();
+                }
+            }
+        }
+    });
 }
