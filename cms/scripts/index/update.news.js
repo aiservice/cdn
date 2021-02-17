626dca7ade146a64846975c86a7331c69673e74c
yangwu3 <yangwu3@cisco.com>
2021-02-17 17:58:17 +0800

    3s to 2s

--- cms/scripts/index/update.news.js
+++ cms/scripts/index/update.news.js
@@ -1,5 +1,5 @@
 /*!
  * Powered by uglifiyJS v2.6.1, Build by http://www.aizhan.club
- * build time: Tue Feb 16 2021 20:54:15 GMT+0800 (China Standard Time)
+ * build time: Wed Feb 17 2021 17:57:42 GMT+0800 (China Standard Time)
 */
-function updateViewCount(){$.post("/CsAjax.do?method=updateNewsViewCount",{news_id:$("#news_id").val()},function(e){if(e.enable_pay&&"undefined"!=typeof site_id&&1772===site_id)if("true"===e.enable_pay){var t=$("#no_source");t.length>0&&(t.after(paySourceHtml()),t.remove())}else{var n=$("#pay_source");n.length>0&&(n.after(noSourceHtml()),n.remove())}})}$(document).ready(function(){setTimeout(updateViewCount,3e3)});
+function updateViewCount(){$.post("/CsAjax.do?method=updateNewsViewCount",{news_id:$("#news_id").val()},function(e){if(e.enable_pay&&"undefined"!=typeof site_id&&1772===site_id)if("true"===e.enable_pay){var t=$("#no_source");t.length>0&&(t.after(paySourceHtml()),t.remove())}else{var n=$("#pay_source");n.length>0&&(n.after(noSourceHtml()),n.remove())}})}$(document).ready(function(){setTimeout(updateViewCount,2e3)});
