e12a38611405ba3e87da5ad1da153868634f448c
yw3 <yw3@abc.com>
2022-07-16 09:36:56 +0800

    2022071609_commit

--- auto.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-commit_name="`date +%Y%m%d`_commit"
-echo "====git auto push start..."
-git add .
-git commit -m $commit_name
-git push origin master
-echo "====git auto push end..."
-
--- os/static/js/ads/top.js
+++ os/static/js/ads/top.js
@@ -1,5 +1,5 @@
 if (typeof site_enabled_g != "undefined" && site_enabled_g) {
     document.write('<div class="text-center" style="margin-left: 0;margin-right: 0">');
-    loadGoogleAds();
+    loadGoogleAds()
     document.write('</div>');
 }
